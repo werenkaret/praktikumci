07e1d8ccc73170fe2b99e114338aa2bf0bf8e2b4
werenkaret <lyfornshora76@gmail.com>
2021-10-18 08:58:46 +0700

    first commit

--- app/Views/template/body.php
+++ app/Views/template/body.php
@@ -2,18 +2,17 @@
         <!-- Navigation-->
         <nav class="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
             <div class="container">
-                <a class="navbar-brand" href="#page-top"><img src="assets/img/navbar-logo.svg" alt="..." /></a>
+                <a class="navbar-brand" href="#page-top"><img src="assets/img/lolele3.png" alt="logo" /></a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
                     Menu
                     <i class="fas fa-bars ms-1"></i>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarResponsive">
                     <ul class="navbar-nav text-uppercase ms-auto py-4 py-lg-0">
-                        <li class="nav-item"><a class="nav-link" href="#services">Services</a></li>
-                        <li class="nav-item"><a class="nav-link" href="#portfolio">Portfolio</a></li>
-                        <li class="nav-item"><a class="nav-link" href="#about">About</a></li>
-                        <li class="nav-item"><a class="nav-link" href="#team">Team</a></li>
-                        <li class="nav-item"><a class="nav-link" href="#contact">Contact</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#services">Produk</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#portfolio">Informasi</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#team">Join Us</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#contact">Kontak</a></li>
                     </ul>
                 </div>
             </div>
@@ -21,9 +20,9 @@
         <!-- Masthead-->
         <header class="masthead">
             <div class="container">
-                <div class="masthead-subheading">Welcome To Our Studio!</div>
-                <div class="masthead-heading text-uppercase">It's Nice To Meet You</div>
-                <a class="btn btn-primary btn-xl text-uppercase" href="#services">Tell Me More</a>
+                <div class="masthead-subheading">Welcome To Our Store!</div>
+                <div class="masthead-heading text-uppercase">Lele berkualitas SUPER dengan harga lemper!</div>
+                <a class="btn btn-primary btn-xl text-uppercase" href="#services">Pelajari selengkapnya</a>
             </div>
         </header>
         <!-- Services-->
